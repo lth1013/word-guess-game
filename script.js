79455f95f50c153ac7ab4873442a33dd1b4e0b5a
unknown <lukeharriman1013@gmail.com>
2023-07-05 14:16:12 -0700

    finished js

--- script.js
+++ script.js
@@ -6,8 +6,66 @@ let winCount = 0;
 let lossCount = 0;
 let timerInterval;
 
-function chooseWord() {
-    chosenWord = words[Math.floor(Math.random() * words.length)];
-    blanks = Array(chosenWord.length).fill("_");
-    document.getElementById("word-blanks").textContent = blanks.join(" ");
+
+const winsDisplay = document.getElementById('wins');
+const lossesDisplay = document.getElementById('losses');
+const wordDisplay = document.getElementById('word-display');
+const messageDisplay = document.getElementById('message');
+const startBtn = document.getElementById('start-btn');
+
+
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
   }
+
+function initGame() {
+    chosenWord = getRandomWord();
+    blanks = Array(chosenWord.length).fill('_');
+    winsDisplay.textContent = wins;
+    lossesDisplay.textContent = losses;
+    wordDisplay.textContent = blanks.join(' ');
+    messageDisplay.textContent = '';
+  }
+
+  function updateBlanks(letter) {
+    for (let i = 0; i < chosenWord.length; i++) {
+      if (chosenWord[i] === letter) {
+        blanks[i] = letter;
+      }
+    }
+    wordDisplay.textContent = blanks.join(' ');
+  }
+
+  function checkWin() {
+    if (blanks.join('') === chosenWord) {
+      wins++;
+      messageDisplay.textContent = 'Congratulations! You won!';
+      clearInterval(timerInterval);
+    }
+  }
+
+  function checkLoss() {
+    losses++;
+    messageDisplay.textContent = 'Oops! Time ran out. Try again!';
+  }
+
+  function startGame() {
+    clearInterval(timerInterval);
+    initGame();
+    timerInterval = setInterval(checkLoss, 10000); // Timer set to 10 seconds (adjust as needed)
+  }
+
+  function handleKeyPress(event) {
+    const guess = event.key.toLowerCase();
+    if (guess.match(/[a-z]/) && !blanks.includes(guess)) {
+      if (chosenWord.includes(guess)) {
+        updateBlanks(guess);
+        checkWin();
+      }
+    }
+  }
+
+  startBtn.addEventListener('click', startGame);
+  document.addEventListener('keydown', handleKeyPress);
+
+  initGame(); //
